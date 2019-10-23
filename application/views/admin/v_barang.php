69c59a2fbbfce9ba51122b3886cbd4d66e0804eb
estigadotnet@lenovo11014isk <project@estiga.net>
2019-10-23 15:16:25 +0700

    .

--- application/views/admin/v_barang.php
+++ application/views/admin/v_barang.php
@@ -13,8 +13,8 @@
 
     <!-- Bootstrap Core CSS -->
     <link href="<?php echo base_url().'assets/css/bootstrap.min.css'?>" rel="stylesheet">
-	<link href="<?php echo base_url().'assets/css/style.css'?>" rel="stylesheet">
-	<link href="<?php echo base_url().'assets/css/font-awesome.css'?>" rel="stylesheet">
+	  <link href="<?php echo base_url().'assets/css/style.css'?>" rel="stylesheet">
+    <link href="<?php echo base_url().'assets/css/font-awesome.css'?>" rel="stylesheet">
     <!-- Custom CSS -->
     <link href="<?php echo base_url().'assets/css/4-col-portfolio.css'?>" rel="stylesheet">
     <link href="<?php echo base_url().'assets/css/dataTables.bootstrap.min.css'?>" rel="stylesheet">
